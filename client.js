b94dac76a3040a1cbdf9e36823d9029e13725e89
Paul Heggeseth <pheggeseth@gmail.com>
2018-07-30 13:44:55 -0500

    removed superfluous functions for readability

--- client.js
+++ client.js
@@ -1,31 +1,23 @@
 let generateClickCount = 0;
 
-function handleGenerateClick() {
-   $('body').append(
-      `<div class="generated">
-         <p>${generateClickCount += 1}</p>
-         <button class="swap-btn">Swap</button>
-         <button class="delete-btn">Delete</button>
-      </div>`
-   );
-}
-
-function handleSwapClick() {
-   $(this).parent().toggleClass('yellow');
-}
-
-function handleDeleteClick() {
-   $(this).parent().remove();
-}
-
-function addEventListeners() {
-   $('#generate-btn').on('click', handleGenerateClick);
-   $('body').on('click', '.swap-btn', handleSwapClick);
-   $('body').on('click', '.delete-btn', handleDeleteClick);
-}
-
 function readyNow() {
-   addEventListeners();
+   $('#generate-btn').on('click', function() {
+      $('body').append(
+         `<div class="generated">
+            <p>${generateClickCount += 1}</p>
+            <button class="swap-btn">Swap</button>
+            <button class="delete-btn">Delete</button>
+         </div>`
+      );
+   });
+   
+   $('body').on('click', '.swap-btn', function() {
+      $(this).parent().toggleClass('yellow');
+   });
+   
+   $('body').on('click', '.delete-btn', function() {
+      $(this).parent().remove();
+   });
 }
 
 $(readyNow);
